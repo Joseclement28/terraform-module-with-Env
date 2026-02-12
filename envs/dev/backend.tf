de8d94c0e4b638f7fe5ec6f81096ba58e850bc22
Joseclement28 <joseclement2807@gmail.com>
2026-02-12 12:09:19 +0530

    backend changed

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-demo-123"
+    bucket         = "my-terraform-state-demo-123-456"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
